175c59dd39a41ec335e4bd0e8d9efc6e82703c7f
RamVikram007 <podishettivikramachary@gmail.com>
2026-04-01 02:55:35 +0530

    Add files via upload

---
+++ dbt_practice/models/silver/silver_salesinfo.sql
@@ -0,0 +1,43 @@
+with sales as (
+    select 
+        sales_id, 
+        product_sk,
+        customer_sk,
+        {{ multiply('unit_price', 'quantity') }} as calculated_gross_amount,
+        gross_amount,
+        payment_method
+    from {{ ref('bronze_sales') }}
+),
+product as (
+    select 
+        product_sk,
+        category
+    from {{ ref('bronze_product') }}
+),
+customer as (
+    select 
+        customer_sk,
+        gender
+    from {{ ref('bronze_customer') }}
+),
+joined_query as (
+select 
+    sales.sales_id,
+    sales.gross_amount,
+    sales.payment_method,
+    product.category,
+    customer.gender
+from sales
+JOIN product
+    ON sales.product_sk = product.product_sk
+JOIN customer
+    ON sales.customer_sk = customer.customer_sk
+)
+
+select
+    category,
+    gender,
+    sum(gross_amount) as total_gross_amount
+from joined_query
+group by category, gender
+order by total_gross_amount desc
